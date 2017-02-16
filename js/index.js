0ec634bafc7ca1659beb94bd998a726d794b0a12
Miguel Carvajal <krvajal.miguelangel@gmail.com>
2017-02-15 22:08:14 -0300

    WIP link model to form input

--- js/index.js
+++ js/index.js
@@ -1,19 +1,41 @@
 
-angular.module('MyApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
+angular.module('PomodoroApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
+.controller('AppCtrl', function($scope, $interval) {
 
-.controller('AppCtrl', function($scope, $timeout) {
-
+  var self = this;
   $scope.timerRunning = false;
   const progressElement =   document.querySelector(".progress");
+  self.currentTimeInSeconds = 25 * 60;   // 25 mins
 
+  var stop;
 
-  $scope.toggleStatus =function(){
+  self.startTimer = function(){
+      stop = $interval(function(){
+          console.log(self.currentTimeInSeconds);
+          self.currentTimeInSeconds--;
+
+      }, 1000, self.currentTimeInSeconds);
+  }
+  self.stopTimer = function(){
+      if(stop){
+        $interval.cancel(stop);
+        stop = undefined;
+      }
+  }
+
+  $scope.toggleStatus = function(){
     progressElement.classList.toggle("playing-session");
       $scope.timerRunning = !$scope.timerRunning;
-
+      if($scope.timerRunning){
+          // continue count down
+          self.startTimer();
+      }else{
+         self.stopTimer();
+      }
   }
-  var config={
+  self.config = {
     sessionLength: {hour: 0, min: 25, sec: 0},
-    breakLenght: {hour: 0, min: 5, sec: 0}
+    breakLength: {hour: 0, min: 5, sec: 0}
   };
+
 });
